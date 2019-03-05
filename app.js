6a19e54161c8036bf67e570c00d591f1496a6572
Ronald Opara <xspazr@gmail.com>
2019-03-04 19:15:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const nameInput = document.getElementById('name');
 const messageTextArea = document.getElementById('message');
+
+nameInput.addEventListener('click', event => {
+    event.target.className = 'highlight';
+});
